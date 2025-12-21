ae5f41d954475e3c62e054c423e748e3c2ef32bb
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-21 01:46:27 +0100

    update

--- Payloadex/Ms/main.c
+++ Payloadex/Ms/main.c
@@ -56,7 +56,7 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     findBootFunctions();
     
     // patch sceboot
-    patchBootPSP();
+    patchBootPSP(&UnpackBootConfigPSP_ARK);
     
     // Forward Call
     return sceReboot(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
