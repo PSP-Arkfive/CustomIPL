d63a939b6a6ea5de747fa82b1f3003d6debad62d
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-01-11 19:51:24 +0100

    cleanup

--- NewIPL/ipl_stage2_payload/main.c
+++ NewIPL/ipl_stage2_payload/main.c
@@ -2,10 +2,6 @@
 #include <syscon.h>
 #include <comms.h>
 #include "gpio.h"
-#ifdef DEBUG
-#include <uart.h>
-#include <printf.h>
-#endif
 
 #ifdef MSIPL
 #include "../../Payloadex/Ms/payload.h"
@@ -17,26 +13,8 @@ void Dcache();
 void Icache();
 int lzo1x_decompress(void*, int, void*, int*, void*);
 
-void *memcpy(void *dest, void *src, uint32_t size)
-{
-    uint32_t *d = (uint32_t *) dest;
-    uint32_t *s = (uint32_t *) src;
-    
-    size /= 4;
-    
-    while (size--)
-        *(d++) = *(s++);
-
-    return dest;
-}
-
 int main()
 {
-#ifdef DEBUG
-    uart_init();
-    printf("stage2 starting...\n");
-#endif
-
     int len = 0x20000;
     lzo1x_decompress(payloadex, size_payloadex, (void*)0x8FC0000, &len, (void*)0);
 
